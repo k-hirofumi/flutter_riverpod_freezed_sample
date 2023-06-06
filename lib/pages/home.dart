e11db1038e43652c08b8df84aefceeeaaa02fada
k-hirofumi <past.is.true@gmail.com>
2023-06-07 01:08:28 +0900

    オーバーレイスピナーをテスト実装

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,6 +11,7 @@ class Home extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     // final userNotifier = ref.watch(userStateNotifierProvider.notifier);
     // final user = ref.watch(userStateNotifierProvider);
+    final overlay = LoadingOverlay.of(context);
 
     return Scaffold(
         appBar: AppBar(),
@@ -72,9 +73,52 @@ class Home extends ConsumerWidget {
                         .watch(userStateNotifierProvider.notifier)
                         .fetchDataAndUpdateUser(),
                     child: const Text('setEmail')),
+
+                ElevatedButton(
+                    onPressed: () async => await overlay
+                        .during(Future.delayed(const Duration(seconds: 2))),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.red),
+                    ),
+                    child: const Text('send!')),
               ],
             ),
           ),
         ));
+  }
+}
+
+class LoadingOverlay {
+  BuildContext _context;
+
+  void hide() {
+    Navigator.of(_context).pop();
+  }
+
+  void show() {
+    showDialog(
+        context: _context,
+        barrierDismissible: false,
+        builder: (ctx) => _FullScreenLoader());
+  }
+
+  Future<T> during<T>(Future<T> future) {
+    show();
+    return future.whenComplete(() => hide());
+  }
+
+  LoadingOverlay._create(this._context);
+
+  factory LoadingOverlay.of(BuildContext context) {
+    return LoadingOverlay._create(context);
+  }
+}
+
+class _FullScreenLoader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: const BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.5)),
+        child: const Center(child: CircularProgressIndicator()));
   }
 }
